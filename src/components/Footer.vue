0ce4bb32334375f2e1c5832da551eb1ed7e033ec
hiro0218 <hiro0218@gmail.com>
2018-10-28 17:38:01 +0900

    create

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    Copyright © <a href="https://b.0218.jp/" target="_blank">hiro</a>. All Rights Reserved.
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <h1>来葉峠の真実</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style>
+</style>
