5de40fbc74fe29038c20e29d85aefd54a8e86a30
hiro0218 <hiro0218@gmail.com>
2018-11-05 18:01:53 +0900

    fix: ベースのスタイルを適応

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="timeline-container">
-    <template v-for="(timeLine, year) in timeLineData">
-      <div class="heading-year" :key="year">{{ year }}</div>
+  <div class="timeline-container">
+    <section v-for="(timeLine, year) in timeLineData" :key="year" class="year-container">
+      <div class="heading-year">{{ year }}</div>
       <article v-for="(article, index) in timeLine" :key="year + index" class="case-data">
-        <h2>{{ article.title }}</h2>
+        <h2 class="article-title">{{ article.title }}</h2>
         <blockquote v-if="article.summary" v-html="article.summary"/>
         <div class="anime-list">
           <h3>アニメ</h3>
@@ -17,16 +17,18 @@
             <li v-for="(comicTitle, comicKey) in article.comic" :key="comicKey">{{ comicTitle }}</li>
           </ul>
         </div>
-        <div class="production-list">
+        <div class="product-list">
           <ul>
-            <li v-for="(production, productionKey) in article.production" :key="productionKey">
-              <a :href="production.url" target="_blank"><img :src="production.img"></a>
+            <li v-for="(product, productKey) in article.product" :key="productKey">
+              <a :href="product.url" target="_blank">
+                <img :src="product.img">
+              </a>
             </li>
           </ul>
         </div>
       </article>
-    </template>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -42,5 +44,59 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.timeline-container {
+  width: 90%;
+  max-width: 1170px;
+  margin: 0 auto;
+}
+
+.heading-year {
+  margin: 1rem 0;
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+.case-data {
+  padding: 1rem;
+  margin-bottom: 2rem;
+  border-radius: 0.25rem;
+  background: #fff;
+  box-shadow: 0 2px 0 #d7e4ed;
+
+  .article-title {
+    margin-bottom: 1rem;
+    letter-spacing: 0.05em;
+    line-height: 1.8;
+  }
+
+  blockquote {
+    margin: 1rem 0 1.5rem;
+    text-align: justify;
+  }
+}
+
+.product-list {
+  ul {
+    display: flex;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  li {
+    & + li {
+      margin-left: 0.5rem;
+    }
+  }
+
+  a {
+    display: block;
+  }
+
+  img {
+    height: 200px;
+    width: 130px;
+  }
+}
 </style>
